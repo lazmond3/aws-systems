c9a1a8419ccb9dc3d3dd81fa3c103a7243c7f7a1
lazmond3 <moikilo00@gmail.com>
2022-05-03 23:32:25 +0900

    [.gitignore] add run

---
+++ run/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tfstate-bucket-mysql"
+    key    = "tfstate-bucket-mysql/tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+module rds {
+    source = "../module/rds"
+    vpc_id = var.vpc_id
+    password = var.password
+}
