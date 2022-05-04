cb92e7551651191119ad7096cb0914b9f9ec78f7
lazmond3 <moikilo00@gmail.com>
2022-05-05 00:10:15 +0900

    [main.tf] elasticsearch-domain

--- run/main.tf
+++ run/main.tf
@@ -21,3 +21,17 @@ module "rds" {
   vpc_id   = var.vpc_id
   password = var.mysql_password
 }
+
+
+resource "aws_elasticsearch_domain" "discord_log" {
+  domain_name           = "discord-log"
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+    instance_type = "r4.large.elasticsearch"
+  }
+
+  tags = {
+    Domain = "discord-log"
+  }
+}
